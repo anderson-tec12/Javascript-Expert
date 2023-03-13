cdad99ac2f9cd5741429615f62ce33bd31f1564a
Anderson Barros Silva <anderson.tec12@gmail.com>
2023-03-13 20:47:34 -0300

    feat: proccess csv

--- 01-testing/01-mocks/src/file.js
+++ 01-testing/01-mocks/src/file.js
@@ -1,5 +1,6 @@
 const {readFile} = require("fs/promises")
 const {join} = require("path")
+const { error } = require("./constants")
 
 const DEFAULT_OPTION = {
   maxLines: 3,
@@ -8,16 +9,70 @@ const DEFAULT_OPTION = {
 
 class File{
   static async csvToJSON(filePath){
-    // const content = await File.getFileContent(filePath)
+    // this is function to return string
+    const content = await readFile(filePath, "utf8")
+    console.log({'file': content})
 
-    // return content
+    const validation = this.isValid(content)
+  
+    if(!validation.valid) throw new Error(validation.error)
+    const result = this.paserCSVToJSON(content)
+    return result
   }
 
+  static isValid(csvString, options = DEFAULT_OPTION){
+    const result = csvString.split(/\r?\n/)
+    console.log({result})
 
-  // static async getFileContent(filePath){
-  //   const filename = join(__dirname, filePath)
-  //   return (await readFile(filename)).toString("utf8")
-  // }
+    const [header, ...fileWithoutHeader] = result
+    console.log({
+      header:header,
+      lines:fileWithoutHeader
+    })
+
+    const isHeaderValid = header === options.fields.join(',')
+
+    if(!isHeaderValid){
+      return {
+        error:error.FILE_FIELDS_ERROR_MESSAGE,
+        valid:false
+      }
+    }
+
+    if(!fileWithoutHeader.length || fileWithoutHeader.length > options.maxLines){
+      return {
+        error: error.FILE_LENGTH_ERROR_MESSAGE,
+        valid:false
+      }
+    }
+
+
+    return {
+      valid:true
+    }
+  }
+
+  static paserCSVToJSON(CSVString){
+    const lines =   CSVString.split(/\r?\n/)
+    const firstLine = lines.shift()
+    const header = firstLine.split(',')
+
+    console.log({header})
+
+    const users = lines.map(line => {
+      const columns = line.split(',')
+      const user = {}
+
+      for(const index in columns){
+        user[header[index]] = columns[index].trim()
+      }
+
+      return user
+    })
+
+    console.log({users})
+    return users
+  }
 
 }
 
