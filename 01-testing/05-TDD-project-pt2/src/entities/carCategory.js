5a1518bfb03d130455fc7ec30d7118142c845405
Anderson Barros Silva <anderson.tec12@gmail.com>
2023-03-18 08:32:47 -0300

    feat: start cap

---
+++ 01-testing/05-TDD-project-pt2/src/entities/carCategory.js
@@ -0,0 +1,13 @@
+const Base = require('./base/base')
+
+class CarCategory extends Base {
+  constructor({id, name, cardIds, price}){
+    super({id, name})
+
+    this.cardIds = cardIds 
+    this.price = price
+
+  }
+}
+
+module.exports = CarCategory
