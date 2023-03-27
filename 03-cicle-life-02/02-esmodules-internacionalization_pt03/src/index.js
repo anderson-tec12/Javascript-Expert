3d39f66a3cc296a7d0f283445c1b05f72077576e
Anderson Barros Silva <anderson.tec12@gmail.com>
2023-03-27 19:25:51 -0300

    feat: step 3

---
+++ 03-cicle-life-02/02-esmodules-internacionalization_pt03/src/index.js
@@ -0,0 +1,37 @@
+import database from '../database.json'
+import TerminalController from './terminalController.js'
+import Person from './person.js'
+
+// 1 C3,CORSA 10000 2023-03-01 2023-03-15
+
+const DEFAULT_LANG = "pt-BR"
+const STOP_TERM = ":q"
+
+const terminalController = new TerminalController()
+terminalController.initializeTerminal(database, DEFAULT_LANG)
+
+
+async function mainLoop(){
+  try{
+    const answer = await terminalController.question(' ')
+    
+    if(answer === STOP_TERM) {
+      terminalController.closeTerminal()
+      console.log('Proccess finished!')
+      return
+    }
+    const person = Person.generateInstanceFromString(answer)
+    terminalController.updateTable(person.formatted(DEFAULT_LANG))
+    // console.log(person.formatted(DEFAULT_LANG))
+
+    return mainLoop()
+  }catch(error){
+    console.error('Deu Ruim', error)
+    return mainLoop()
+  }
+}
+
+
+await mainLoop()
+
+
