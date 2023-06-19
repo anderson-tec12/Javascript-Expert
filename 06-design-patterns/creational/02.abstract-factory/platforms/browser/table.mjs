6417cbef20ee7846d206f9675d63b6ef2a4d2856
Anderson Barros Silva <anderson.tec12@gmail.com>
2023-06-19 19:46:06 -0300

    Iniciando o padrão abstract factory

---
+++ 06-design-patterns/creational/02.abstract-factory/platforms/browser/table.mjs
@@ -0,0 +1,7 @@
+import TableComponent from '../../shared/base/tableComponent.mjs';
+
+export default class TableBrwoserComponent extends TableComponent{
+  render(data){
+    window.alert('Chamou!!')
+  }
+}
