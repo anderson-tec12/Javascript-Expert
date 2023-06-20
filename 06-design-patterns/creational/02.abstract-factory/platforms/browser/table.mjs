b01ee74ba26d0e91e7b22b03d8e6dcad2b728799
Anderson Barros Silva <anderson.tec12@gmail.com>
2023-06-19 21:59:46 -0300

    finalização do app

--- 06-design-patterns/creational/02.abstract-factory/platforms/browser/table.mjs
+++ 06-design-patterns/creational/02.abstract-factory/platforms/browser/table.mjs
@@ -2,6 +2,37 @@ import TableComponent from '../../shared/base/tableComponent.mjs';
 
 export default class TableBrwoserComponent extends TableComponent{
   render(data){
-    window.alert('Chamou!!')
+   const tmplate = this.prepareData(data)
+
+   document.body.insertAdjacentHTML("afterbegin", tmplate)
+  }
+
+  prepareData(data){
+    const [firstItem] = data
+    const theaders = Object.keys(firstItem)
+                           .map(text => `<th scope="col">${text}</th>`)
+    
+    const joinLists = list => list.join('')   
+
+    const tbodyValues = data
+      .map(item => Object.values(item))
+      .map(item => item.map(value => `<td>${value}</td>`))
+      .map(tds => `<tr>${joinLists(tds)}</tr>`)
+    
+    const template = `
+      <table class="table">
+        <thead>
+          <tr>
+            ${joinLists(theaders)}
+          </tr>
+        </thead>
+        <tbody>
+          ${joinLists(tbodyValues)}
+        </tbody>
+      </table>
+    `
+
+    return template
+
   }
 }
