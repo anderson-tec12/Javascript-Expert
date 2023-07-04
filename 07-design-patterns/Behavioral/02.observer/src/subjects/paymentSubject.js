df931e2efac4b4e1dfe2af3787943009f5d79bdc
Anderson Barros Silva <anderson.tec12@gmail.com>
2023-07-03 22:05:40 -0300

    Implementando testes

--- 07-design-patterns/Behavioral/02.observer/src/subjects/paymentSubject.js
+++ 07-design-patterns/Behavioral/02.observer/src/subjects/paymentSubject.js
@@ -1,14 +1,18 @@
 export default class PaymentSubject{
-    notify(data){
 
+    #obsersers = new Set()
+    notify(data){
+        this.#obsersers.forEach(observer => {
+            observer.update(data)
+        })
     }
 
     subscribe(observable){
-
+        this.#obsersers.add(observable)
     }
 
     unsubscribe(observable){
-
+        this.#obsersers.delete(observable)
     }
 }
 
--- 07-design-patterns/Behavioral/02.observer/test/index.test.js
+++ 07-design-patterns/Behavioral/02.observer/test/index.test.js
@@ -1,5 +1,38 @@
 import {expect, describe,test,jest} from '@jest/globals'
+import PaymentSubject from '../src/subjects/paymentSubject.js'
 
 describe('Test Suite for Observer Pattern', () => {
-    test.todo('#Payment Subject notify observers')
+    test.todo('#Payment Subject notify observers', () => {
+        const  subject = new PaymentSubject()
+        const observer = {
+            update: jest.fn()
+        }
+
+        const data = 'hello world'
+        const expected = data
+
+        subject.subscribe(observer)
+        subject.notify(data)
+
+        expect(observer.update).toBeCalledWith(expected)
+    })
+
+    test.todo('#Payment Subject should not notify unsubscribed observers', () => {
+        const  subject = new PaymentSubject()
+        const observer = {
+            update: jest.fn()
+        }
+
+        const data = 'hello world'
+
+        subject.subscribe(observer)
+        subject.unsubscribe(observer)
+        subject.notify(data)
+
+        expect(observer.update).not.toHaveBeenCalled()
+    })
+
+    test.todo('#Payment should notify subject after a credit card transaction')
+    
+    test.todo('#All should notify subscribers after a crdit card payment')
 })
