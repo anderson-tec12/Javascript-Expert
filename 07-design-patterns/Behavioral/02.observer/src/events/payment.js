9ee7c406be68a326bc932a17b9151de2113bf16d
Anderson Barros Silva <anderson.tec12@gmail.com>
2023-07-08 00:29:40 -0300

    implementando areas que devem ser notificadas

--- 07-design-patterns/Behavioral/02.observer/src/events/payment.js
+++ 07-design-patterns/Behavioral/02.observer/src/events/payment.js
@@ -1,5 +1,10 @@
 export default class Payment{
+    constructor(paymentSubject){
+        this.paymentSubject = paymentSubject
+    }
+
     creditCard(paymentData){
         console.log(`\na payment ocurred from ${paymentData.useName}`)
+        this.paymentSubject.notify(paymentData)
     }
 }
--- 07-design-patterns/Behavioral/02.observer/test/index.test.js
+++ 07-design-patterns/Behavioral/02.observer/test/index.test.js
@@ -1,8 +1,12 @@
-import {expect, describe,test,jest} from '@jest/globals'
+import {expect, describe,test,jest, beforeAll} from '@jest/globals'
 import PaymentSubject from '../src/subjects/paymentSubject.js'
 
 describe('Test Suite for Observer Pattern', () => {
-    test.todo('#Payment Subject notify observers', () => {
+    beforeAll(() => {
+        jest.spyOn(console, console.log.name).mockImplementation(() => {})
+    })
+
+    test('#Payment Subject notify observers', () => {
         const  subject = new PaymentSubject()
         const observer = {
             update: jest.fn()
@@ -17,7 +21,7 @@ describe('Test Suite for Observer Pattern', () => {
         expect(observer.update).toBeCalledWith(expected)
     })
 
-    test.todo('#Payment Subject should not notify unsubscribed observers', () => {
+    test('#Payment Subject should not notify unsubscribed observers', () => {
         const  subject = new PaymentSubject()
         const observer = {
             update: jest.fn()
@@ -32,7 +36,21 @@ describe('Test Suite for Observer Pattern', () => {
         expect(observer.update).not.toHaveBeenCalled()
     })
 
-    test.todo('#Payment should notify subject after a credit card transaction')
+    test('#Payment should notify subject after a credit card transaction', () => {
+        const subject = new PaymentSubject()
+        const payment = new Payment(subject)
+
+        const paymentSubjectNotifierSpy = jest.spyOn(
+            payment.paymentSubject,
+            payment.paymentSubject.notify.name
+        )
+
+        const data = {userName:'Anderson B. Silva', id: Date.now()}
+        payment.creditCard(data)
+
+        expect(paymentSubjectNotifierSpy).toBeCalledWith(data)
+
+    })
     
     test.todo('#All should notify subscribers after a crdit card payment')
 })
